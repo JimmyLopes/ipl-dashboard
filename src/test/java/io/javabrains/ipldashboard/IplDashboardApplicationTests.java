a11c7ee24a1045ad94ed4c34cb6f1a510653f918
Jimmy Botezine <jbotezine@gmail.com>
2021-11-08 20:24:50 -0300

    Inital Structure

---
+++ src/test/java/io/javabrains/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.javabrains.ipldashboard;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//not implemented
+		Assertions.assertTrue(true);
+	}
+
+}
